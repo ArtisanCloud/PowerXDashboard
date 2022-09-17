d2fbe6a0a72e6cd446392699393f2d58ea805094
Matrix-X <matrix-x@artisan-cloud.com>
2022-09-18 00:23:52 +0800

    feat(logo): replace artisan cloud logo and arvatar

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
 // 运行时配置
 
+import LogoSVG from '@/assets/logo.svg';
+import LogoAvatar from '@/assets/logo.png';
+
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://next.umijs.org/docs/api/runtime-config#getinitialstate
-export async function getInitialState(): Promise<{ name: string }> {
-  return { name: '@umijs/max' };
+export async function getInitialState(): Promise<{
+  name: string;
+  avatar?: string;
+}> {
+  return {
+    name: '@ArtisanCloud/PowerX',
+    avatar: LogoAvatar,
+  };
 }
 
 export const layout = () => {
   return {
-    logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+    logo: LogoSVG,
     menu: {
       locale: false,
     },
