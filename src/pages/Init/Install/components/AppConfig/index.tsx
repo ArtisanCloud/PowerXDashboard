c2613bfebd33b66877a87b4565fd4915a8d384fa
Matrix-X <matrix-x@artisan-cloud.com>
2022-10-11 11:15:39 +0800

    refactor(install): add grid row and col for form

--- src/pages/Init/Install/components/AppConfig/index.tsx
+++ src/pages/Init/Install/components/AppConfig/index.tsx
@@ -1,38 +1,53 @@
 import { ProForm, ProFormText } from '@ant-design/pro-components';
+import { Col, Row } from 'antd';
 
 export default () => {
+  const rowGutter = 32;
   return (
     <ProForm.Group>
-      <ProFormText
-        name={'name'}
-        label={'系统名称'}
-        tooltip={'比如 - PowerX后台系统'}
-        placeholder={'如：PowerX后台系统'}
-        initialValue={'PowerX后台系统'}
-        rules={[{ required: true }]}
-      />
-      <ProFormText
-        name={'env'}
-        label={'部署环境'}
-        tooltip={'dev | test | production'}
-        placeholder={'如：dev'}
-        initialValue={'dev'}
-        rules={[{ required: true }]}
-      />
-      <ProFormText
-        name={'locale'}
-        label={'本地化'}
-        tooltip={'比如 - zh_CN | en_US'}
-        initialValue={'zh_CN'}
-        placeholder={'当前系统默认zh_CN'}
-      />
-      <ProFormText
-        name={'timezone'}
-        label={'时差'}
-        tooltip={"比如 - 'Asia/Shanghai' | 'America/New_York'"}
-        initialValue={'Asia/Shanghai'}
-        placeholder={"当前系统默认 'Asia/Shanghai'"}
-      />
+      <Row gutter={rowGutter}>
+        <Col>
+          <ProFormText
+            name={'name'}
+            label={'系统名称'}
+            tooltip={'比如 - PowerX后台系统'}
+            placeholder={'如：PowerX后台系统'}
+            initialValue={'PowerX后台系统'}
+            rules={[{ required: true }]}
+          />
+        </Col>
+        <Col>
+          <ProFormText
+            name={'env'}
+            label={'部署环境'}
+            tooltip={'dev | test | production'}
+            placeholder={'如：dev'}
+            initialValue={'dev'}
+            rules={[{ required: true }]}
+          />
+        </Col>
+      </Row>
+
+      <Row gutter={rowGutter}>
+        <Col>
+          <ProFormText
+            name={'locale'}
+            label={'本地化'}
+            tooltip={'比如 - zh_CN | en_US'}
+            initialValue={'zh_CN'}
+            placeholder={'当前系统默认zh_CN'}
+          />
+        </Col>
+        <Col>
+          <ProFormText
+            name={'timezone'}
+            label={'时差'}
+            tooltip={"比如 - 'Asia/Shanghai' | 'America/New_York'"}
+            initialValue={'Asia/Shanghai'}
+            placeholder={"当前系统默认 'Asia/Shanghai'"}
+          />
+        </Col>
+      </Row>
     </ProForm.Group>
   );
 };
--- src/pages/Init/Install/components/WechatConfig/index.tsx
+++ src/pages/Init/Install/components/WechatConfig/index.tsx
@@ -3,119 +3,146 @@ import {
   ProFormText,
   ProFormTextArea,
 } from '@ant-design/pro-components';
+import { Col, Row } from 'antd';
 
 export default () => {
+  const rowGutter = 32;
   return (
     <ProForm.Group>
-      <ProFormText
-        name="corpID"
-        label="企业微信账号"
-        tooltip="请到微信商户号后台配置获取"
-        placeholder="如：ww45xxxxxxxxxxxxxxx"
-        rules={[{ required: true }]}
-      />
-      <ProFormText
-        name="appOauthCallbackURL"
-        label="企业微信Oauth回调地址"
-        tooltip="请到微信商户号后台配置获取"
-        placeholder="如：http://power.artisancloud.cn"
-        rules={[{ required: true }]}
-      />
-      <ProFormText
-        name="weComAgentID"
-        label="企微应用账号"
-        tooltip="请到微信商户号后台配置获取"
-        placeholder="如：10000001"
-        rules={[{ required: true }]}
-      />
-      <ProFormText.Password
-        name="weComSecret"
-        label="企微应用账号密钥"
-        tooltip="请到微信商户号后台配置获取"
-        rules={[{ required: true }]}
-      />
+      <Row gutter={rowGutter}>
+        <Col>
+          <ProFormText
+            name="corpID"
+            label="企业微信账号"
+            tooltip="请到微信商户号后台配置获取"
+            placeholder="如：ww45xxxxxxxxxxxxxxx"
+            rules={[{ required: true }]}
+          />
+        </Col>
+        <Col>
+          <ProFormText
+            name="appOauthCallbackURL"
+            label="企业微信Oauth回调地址"
+            tooltip="请到微信商户号后台配置获取"
+            placeholder="如：http://power.artisancloud.cn"
+            rules={[{ required: true }]}
+          />
+        </Col>
+      </Row>
 
-      <ProFormTextArea
-        name="appCertPublicKey"
-        label="企微应用证书公钥"
-        tooltip="请到微信商户号后台配置获取"
-        placeholder="如：|
+      <Row gutter={rowGutter}>
+        <Col>
+          <ProFormText
+            name="weComAgentID"
+            label="企微应用账号"
+            tooltip="请到微信商户号后台配置获取"
+            placeholder="如：10000001"
+            rules={[{ required: true }]}
+          />
+        </Col>
+        <Col>
+          <ProFormText.Password
+            name="weComSecret"
+            label="企微应用账号密钥"
+            tooltip="请到微信商户号后台配置获取"
+            rules={[{ required: true }]}
+          />
+        </Col>
+      </Row>
+      <Row gutter={rowGutter}>
+        <Col>
+          <ProFormTextArea
+            name="appCertPublicKey"
+            label="企微应用证书公钥"
+            tooltip="请到微信商户号后台配置获取"
+            placeholder="如：|
     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        rules={[{ required: true }]}
-      />
-      <ProFormText.Password
-        name="appMessageAESKey"
-        label="企微应用AES密钥"
-        tooltip="请到微信商户号后台配置获取"
-        rules={[{ required: true }]}
-      />
-      <ProFormText
-        width={'lg'}
-        name="appMessageCallbackURL"
-        label="企微应用消息回调地址"
-        tooltip="请保证host替换成你想要的域名，后面URI为 wechat/api/weCom/app"
-        placeholder="https://{host}/wechat/api/weCom/app"
-        initialValue={'https://{host}/wechat/api/weCom/app'}
-        rules={[{ required: true }]}
-      />
-      <ProFormText.Password
-        width={'lg'}
-        name="appMessageToken"
-        label="企微应用消息Token"
-        tooltip="请到微信商户号后台配置获取"
-        rules={[{ required: true }]}
-      />
+            rules={[{ required: true }]}
+          />
+        </Col>
+      </Row>
 
-      <ProFormText.Password
-        width={'lg'}
-        name="customerMessageAESKey"
-        label="企微客户联系人AES密钥"
-        tooltip="请到微信商户号后台配置获取"
-        rules={[{ required: true }]}
-      />
-      <ProFormText
-        width={'lg'}
-        name="customerMessageCallbackURL"
-        label="企微客户联系人消息回调地址"
-        tooltip="请保证host替换成你想要的域名，后面URI为 wechat/api/weCom/customer"
-        placeholder="https://{host}/wechat/api/weCom/customer"
-        initialValue={'https://{host}/wechat/api/weCom/customer'}
-        rules={[{ required: true }]}
-      />
-      <ProFormText.Password
-        width={'lg'}
-        name="customerMessageToken"
-        label="企微客户联系人消息Token"
-        tooltip="请到微信商户号后台配置获取"
-        rules={[{ required: true }]}
-      />
+      <Row gutter={rowGutter}>
+        <Col>
+          <ProFormText.Password
+            name="appMessageAESKey"
+            label="企微应用AES密钥"
+            tooltip="请到微信商户号后台配置获取"
+            rules={[{ required: true }]}
+          />
+        </Col>
+        <Col>
+          <ProFormText
+            width={'lg'}
+            name="appMessageCallbackURL"
+            label="企微应用消息回调地址"
+            tooltip="请保证host替换成你想要的域名，后面URI为 wechat/api/weCom/app"
+            placeholder="https://{host}/wechat/api/weCom/app"
+            initialValue={'https://{host}/wechat/api/weCom/app'}
+            rules={[{ required: true }]}
+          />
+        </Col>
+        <Col>
+          <ProFormText.Password
+            width={'lg'}
+            name="appMessageToken"
+            label="企微应用消息Token"
+            tooltip="请到微信商户号后台配置获取"
+            rules={[{ required: true }]}
+          />
+        </Col>
+      </Row>
 
-      <ProFormText.Password
-        width={'lg'}
-        name="employeeMessageAESKey"
-        label="企微内部联系人AES密钥"
-        tooltip="请到微信商户号后台配置获取"
-        rules={[{ required: true }]}
-      />
-      <ProFormText
-        width={'lg'}
-        name="employeeMessageCallbackURL"
-        label="企微内部联系人消息回调地址"
-        tooltip="请保证host替换成你想要的域名，后面URI为 wechat/api/weCom/employee"
-        placeholder="https://{host}/wechat/api/weCom/employee"
-        initialValue={'https://{host}/wechat/api/weCom/employee'}
-        rules={[{ required: true }]}
-      />
-      <ProFormText.Password
-        width={'lg'}
-        name="employeeMessageToken"
-        label="企微内部联系人消息Token"
-        tooltip="请到微信商户号后台配置获取"
-        rules={[{ required: true }]}
-      />
+      {/*<ProFormText.Password*/}
+      {/*  width={'lg'}*/}
+      {/*  name="customerMessageAESKey"*/}
+      {/*  label="企微客户联系人AES密钥"*/}
+      {/*  tooltip="请到微信商户号后台配置获取"*/}
+      {/*  rules={[{ required: true }]}*/}
+      {/*/>*/}
+      {/*<ProFormText*/}
+      {/*  width={'lg'}*/}
+      {/*  name="customerMessageCallbackURL"*/}
+      {/*  label="企微客户联系人消息回调地址"*/}
+      {/*  tooltip="请保证host替换成你想要的域名，后面URI为 wechat/api/weCom/customer"*/}
+      {/*  placeholder="https://{host}/wechat/api/weCom/customer"*/}
+      {/*  initialValue={'https://{host}/wechat/api/weCom/customer'}*/}
+      {/*  rules={[{ required: true }]}*/}
+      {/*/>*/}
+      {/*<ProFormText.Password*/}
+      {/*  width={'lg'}*/}
+      {/*  name="customerMessageToken"*/}
+      {/*  label="企微客户联系人消息Token"*/}
+      {/*  tooltip="请到微信商户号后台配置获取"*/}
+      {/*  rules={[{ required: true }]}*/}
+      {/*/>*/}
+
+      {/*<ProFormText.Password*/}
+      {/*  width={'lg'}*/}
+      {/*  name="employeeMessageAESKey"*/}
+      {/*  label="企微内部联系人AES密钥"*/}
+      {/*  tooltip="请到微信商户号后台配置获取"*/}
+      {/*  rules={[{ required: true }]}*/}
+      {/*/>*/}
+      {/*<ProFormText*/}
+      {/*  width={'lg'}*/}
+      {/*  name="employeeMessageCallbackURL"*/}
+      {/*  label="企微内部联系人消息回调地址"*/}
+      {/*  tooltip="请保证host替换成你想要的域名，后面URI为 wechat/api/weCom/employee"*/}
+      {/*  placeholder="https://{host}/wechat/api/weCom/employee"*/}
+      {/*  initialValue={'https://{host}/wechat/api/weCom/employee'}*/}
+      {/*  rules={[{ required: true }]}*/}
+      {/*/>*/}
+      {/*<ProFormText.Password*/}
+      {/*  width={'lg'}*/}
+      {/*  name="employeeMessageToken"*/}
+      {/*  label="企微内部联系人消息Token"*/}
+      {/*  tooltip="请到微信商户号后台配置获取"*/}
+      {/*  rules={[{ required: true }]}*/}
+      {/*/>*/}
 
       {/*	<ProFormText*/}
       {/*		name="authCallbackHost"*/}
