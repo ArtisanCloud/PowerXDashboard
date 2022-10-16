d0c7603c89794759a49c804cba9cc2c109f87493
Matrix-X <matrix-x@artisan-cloud.com>
2022-10-16 17:06:13 +0800

    feat(initRoot): check root init status

--- src/models/global.tsx
+++ src/models/global.tsx
@@ -3,7 +3,10 @@ import { DEFAULT_NAME } from '@/constants';
 import { useEffect, useState } from 'react';
 import { useModel } from 'umi';
 import { message } from 'antd';
-import { CheckSystemInstalled } from '@/services/boot/BootController';
+import {
+  CheckRootInitialized,
+  CheckSystemInstalled,
+} from '@/services/boot/BootController';
 import { API_RETURN_CODE_INIT } from '@/constants/api';
 
 export const UseApp = () => {
@@ -53,6 +56,44 @@ export const UseApp = () => {
     // 返回值
     return () => {};
   }, [sysInstalled]);
+
+  useEffect(() => {
+    const HandleCheckRootInitialized = async () => {
+      // 读取localstorage的rootInitialized
+      const jsonRootInitialized = localStorage.getItem('rootInitialized');
+      if (jsonRootInitialized !== null) {
+        const isRootInitialized: boolean = JSON.parse(jsonRootInitialized!);
+        // 如果系统已经初始化过Root，则直接进入系统
+        if (isRootInitialized) {
+          setRootInitialized(true);
+          return;
+        }
+      }
+
+      // 检查远程是否初始化过Root
+      const rs: API.ResponseRootInitStatus = await CheckRootInitialized();
+      if (rs.meta.return_code === API_RETURN_CODE_INIT) {
+        // check system status
+        if (rs.data !== null) {
+          // console.log(sysInstalled)
+          localStorage.setItem('rootInitialized', 'true');
+          // set status
+          setSystemInstalled(sysInstalled);
+        }
+      } else {
+        message.error(rs.meta.result_message);
+        return;
+      }
+    };
+
+    // 检查后台系统是否被安装
+    HandleCheckRootInitialized().catch((e) => {
+      console.error('HandleCheckRootInitialized', e);
+    });
+
+    // 返回值
+    return () => {};
+  }, [rootInitialized]);
 
   useEffect(() => {
     const HandleCheckRootInitialized = async () => {
--- src/pages/Init/Root/index.tsx
+++ src/pages/Init/Root/index.tsx
@@ -1,8 +1,8 @@
 import { PageContainer } from '@ant-design/pro-components';
-import styles from './index.less';
 import { UseApp } from '@/models/global';
 import * as URIConstant from '@/constants/uri';
 import { history } from 'umi';
+import ProCard from '@ant-design/pro-card';
 
 const RootInitPage: React.FC = () => {
   const { rootInitialized } = UseApp();
@@ -13,7 +13,15 @@ const RootInitPage: React.FC = () => {
 
   return (
     <PageContainer ghost>
-      <div className={styles.container}>初始化Root 待开发</div>
+      <ProCard
+        title="初始化系统管理员"
+        extra="请扫码"
+        tooltip="请初始化系统管理员"
+        style={{ maxWidth: 300 }}
+        headerBordered
+      >
+        系统安装完毕后，需要初始化一个Root级别的超级管理员，可以设置系统权限功能等。
+      </ProCard>
     </PageContainer>
   );
 };
