0efd6a6179e65c4ba6428196ed7da9dcac231bef
Northseadl <northseadl@outlook.com>
2023-04-08 12:57:21 +0800

    optimized the routing generation code

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -2,19 +2,21 @@ import type { RouteRecordNormalized } from 'vue-router';
 import { EMPTY_LAYOUT_TAG } from '@/router/constants';
 import { cloneDeep } from 'lodash';
 
+// Import local module routes and external module routes
 const modules = import.meta.globEager('./modules/*.ts');
 const externalModules = import.meta.globEager('./externalModules/*.ts');
 
-function FilterEmptyRoute(moduleRoute: any): any {
+// Function to filter out empty routes from the module routes
+function filterEmptyRoute(moduleRoute: any): any {
   function deepFilterChildren(route: any): any[] {
-    const newChildren = [] as any[];
+    const newChildren: any[] = [];
     if (route.children !== undefined && route.children.length > 0) {
       route.children.forEach((c1: any) => {
         if (
           c1.component.name === EMPTY_LAYOUT_TAG &&
           c1.children !== undefined
         ) {
-          // EmptyLayout 返回其 children
+          // If EmptyLayout, return its children
           c1.children.forEach((c2: any) => {
             if (c2.children !== undefined) {
               c2.children = deepFilterChildren(c2);
@@ -24,7 +26,7 @@ function FilterEmptyRoute(moduleRoute: any): any {
           c1.children = undefined;
           newChildren.push(c1);
         } else {
-          // 非 EmptyLayout 返回其自身
+          // If not EmptyLayout, return itself
           if (c1.children !== undefined) {
             c1.children = deepFilterChildren(c1);
           }
@@ -39,12 +41,13 @@ function FilterEmptyRoute(moduleRoute: any): any {
   return deepFilterChildren(moduleRoute);
 }
 
+// Function to format the imported modules into the expected format for routes
 function formatModules(_modules: any, result: RouteRecordNormalized[]) {
   Object.keys(_modules).forEach((key) => {
     const defaultModule = cloneDeep(_modules[key].default);
     if (!defaultModule) return;
 
-    defaultModule.children = FilterEmptyRoute(defaultModule);
+    defaultModule.children = filterEmptyRoute(defaultModule);
 
     const moduleList = Array.isArray(defaultModule)
       ? [...defaultModule]
@@ -54,6 +57,7 @@ function formatModules(_modules: any, result: RouteRecordNormalized[]) {
   return result;
 }
 
+// Function to format the imported modules into the expected format for menu routes
 function formatModulesForMenu(_modules: any, result: RouteRecordNormalized[]) {
   Object.keys(_modules).forEach((key) => {
     const defaultModule = _modules[key].default;
@@ -67,13 +71,12 @@ function formatModulesForMenu(_modules: any, result: RouteRecordNormalized[]) {
   return result;
 }
 
+// Export the formatted app routes, menu routes, and external app routes
 export const appRoutes: RouteRecordNormalized[] = formatModules(modules, []);
-
 export const menuRoutes: RouteRecordNormalized[] = formatModulesForMenu(
   modules,
   []
 );
-
 export const appExternalRoutes: RouteRecordNormalized[] = formatModules(
   externalModules,
   []
