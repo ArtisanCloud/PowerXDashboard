98f590566f9a072f8f8bf479fd602bb9c9585a5c
Matrix-X <hbj418@gmail.com>
2023-04-19 16:31:37 +0800

    feat(reservation): add calendar component solution

---
+++ src/views/custom/reservation-center/reservation/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script type="ts" setup>
+
+// Calendar Component
+// https://github.com/fullcalendar/fullcalendar-vue
+
+</script>
+
+<style scoped>
+
+</style>
