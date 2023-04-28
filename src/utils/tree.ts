91ea1eb42c928ae7006212dfc9865cbd0e57a670
Matrix-X <hbj418@gmail.com>
2023-04-28 15:31:04 +0800

    fix(api): customer and leads type and status

--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -3,19 +3,19 @@ type TreeNode = {
 	children?: TreeNode[];
 };
 
-function FindNodeById<T extends TreeNode>(tree: T[], id: bigint): T | undefined {
-	for (const node of tree) {
-		if (node.id === id) {
-			return node;
-		}
-
-		if (node.children) {
-			const result = findNodeById(node.children, id);
-			if (result) {
-				return result;
-			}
-		}
-	}
-
-	return undefined;
-}
+// function FindNodeById<T extends TreeNode>(tree: T[], id: bigint): T | undefined {
+// 	for (const node of tree) {
+// 		if (node.id === id) {
+// 			return node;
+// 		}
+//
+// 		if (node.children) {
+// 			const result = findNodeById(node.children, id);
+// 			if (result) {
+// 				return result;
+// 			}
+// 		}
+// 	}
+//
+// 	return undefined;
+// }
