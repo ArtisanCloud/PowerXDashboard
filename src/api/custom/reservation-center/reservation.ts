98483da832332e3f5ce9dfd0632db8f2a3b77398
Matrix-X <hbj418@gmail.com>
2023-04-29 22:09:32 +0800

    feat(customer): crud function

--- src/api/custom/reservation-center/reservation.ts
+++ src/api/custom/reservation-center/reservation.ts
@@ -1,7 +1,7 @@
 import {PowerModel, PREFIX_URI_ADMIN_API} from '@/api/common';
 import axios from "axios";
 import {Artisan} from "@/api/crm/product-service/artisan";
-import {Customer} from "@/api/crm/customerdomain/customer";
+import {Customer} from "@/api/crm/customer-domain/customer";
 import {ServiceSpecific} from "@/api/custom/product-service/serviceSpecific";
 
 
