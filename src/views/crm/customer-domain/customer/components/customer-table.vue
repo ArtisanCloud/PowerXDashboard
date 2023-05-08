7c229189f2adc1764ff9f6c3c629546c3ce78614
Matrix-X <hbj418@gmail.com>
2023-05-08 12:19:06 +0800

    fix(local): title

--- src/views/crm/customer-domain/customer/components/customer-table.vue
+++ src/views/crm/customer-domain/customer/components/customer-table.vue
@@ -168,12 +168,12 @@ const deleteCustomerById = async (bookId: number) => {
 };
 
 const pageChanged = (page: number) => {
-  // console.log("page", page)
+  console.log("page", page)
   fetchCustomerList({pageIndex: page, pageSize: pagination.pageSize})
 }
 
 const pageSizeChanged = (pageSize: number) => {
-  // console.log("pagesize", pageSize)
+  console.log("pagesize", pageSize)
   fetchCustomerList({pageIndex: pagination.currentPage, pageSize})
 }
 
