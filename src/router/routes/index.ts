bf919d71807c1ed9929f0a671a897d6a6ccf1ecc
Northseadl <northseadl@outlook.com>
2023-08-08 00:39:03 +0800

    适配嵌套children路由

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -2,53 +2,46 @@ import type { RouteRecordNormalized } from 'vue-router';
 import { EMPTY_LAYOUT_TAG } from '@/router/constants';
 import { cloneDeep } from 'lodash';
 
-// Import local module routes and external module routes
 const modules = import.meta.globEager('./modules/*.ts');
 const externalModules = import.meta.globEager('./externalModules/*.ts');
 
-// Function to filter out empty routes from the module routes
-function filterEmptyRoute(moduleRoute: any): any {
-  function deepFilterChildren(route: any): any[] {
-    const newChildren: any[] = [];
-    if (route.children !== undefined && route.children.length > 0) {
-      route.children.forEach((c1: any) => {
-        if (
-          c1.component.name === EMPTY_LAYOUT_TAG &&
-          c1.children !== undefined
-        ) {
-          // If EmptyLayout, return its children
-          c1.children.forEach((c2: any) => {
-            if (c2.children !== undefined) {
-              c2.children = deepFilterChildren(c2);
-            }
-            newChildren.push(c2);
-          });
-          c1.children = undefined;
-          newChildren.push(c1);
+// 嵌套EMPTY_LAYOUT路由处理, 该操作是为了适配用于缓存的顶层RouteView, 支持路由嵌套children
+function filterEmptyRoute(moduleRoute: any) {
+  // 如果一个路由的component是EMPTY_LAYOUT_TAG，那么这个路由是空路由, 从叶子向上递归提取children到EMPTY_LAYOUT路由同级别
+  const upEmptyLayoutChildren = (route: any) => {
+    const { children } = route;
+    delete route.children;
+    return [route, ...children];
+  };
+
+  const deepFilterChildren = (routes: any[]) => {
+    const result: any[] = [];
+    routes.forEach((route) => {
+      if (route.children) {
+        route.children = deepFilterChildren(route.children);
+        if (route.component.name === EMPTY_LAYOUT_TAG) {
+          result.push(...upEmptyLayoutChildren(route));
         } else {
-          // If not EmptyLayout, return itself
-          if (c1.children !== undefined) {
-            c1.children = deepFilterChildren(c1);
-          }
-          newChildren.push(c1);
+          result.push(route);
         }
-      });
-    }
+      } else {
+        result.push(route);
+      }
+    });
+    return result;
+  };
 
-    return newChildren;
-  }
-
-  return deepFilterChildren(moduleRoute);
+  moduleRoute.children = deepFilterChildren(moduleRoute.children);
+  return moduleRoute;
 }
 
-// Function to format the imported modules into the expected format for routes
+// 将ModuleRoutes转换为框架应用使用的Routes
 function formatModules(_modules: any, result: RouteRecordNormalized[]) {
   Object.keys(_modules).forEach((key) => {
-    const defaultModule = cloneDeep(_modules[key].default);
+    let defaultModule = cloneDeep(_modules[key].default);
     if (!defaultModule) return;
 
-    defaultModule.children = filterEmptyRoute(defaultModule);
-
+    defaultModule = filterEmptyRoute(defaultModule);
     const moduleList = Array.isArray(defaultModule)
       ? [...defaultModule]
       : [defaultModule];
@@ -57,7 +50,7 @@ function formatModules(_modules: any, result: RouteRecordNormalized[]) {
   return result;
 }
 
-// Function to format the imported modules into the expected format for menu routes
+// 将ModuleRoutes转换为菜单使用的Routes
 function formatModulesForMenu(_modules: any, result: RouteRecordNormalized[]) {
   Object.keys(_modules).forEach((key) => {
     const defaultModule = _modules[key].default;
@@ -71,12 +64,15 @@ function formatModulesForMenu(_modules: any, result: RouteRecordNormalized[]) {
   return result;
 }
 
-// Export the formatted app routes, menu routes, and external app routes
+// 框架应用使用的Routes
 export const appRoutes: RouteRecordNormalized[] = formatModules(modules, []);
+
+// 用于显示菜单的Route列表
 export const menuRoutes: RouteRecordNormalized[] = formatModulesForMenu(
   modules,
   []
 );
+
 export const appExternalRoutes: RouteRecordNormalized[] = formatModules(
   externalModules,
   []
