cf971975c276556162235c172f044d3503ea4eba
Matrix-X <hbj418@gmail.com>
2023-08-18 00:42:40 +0800

    fix(crm): product add or remove with exist cover and detail images

--- src/utils/sort-id-item.ts
+++ src/utils/sort-id-item.ts
@@ -10,3 +10,16 @@ export const sortBySortIndexAndGetSortedIds = (
   const sortedIds = sortedArray.map((item) => item.id);
   return sortedIds;
 };
+
+export const rebuildSortIndex = (array: SortIdItem[]) => {
+  array.forEach((item: SortIdItem, index: number) => {
+    item.sortIndex = index; // 更新 sortIndex 为当前索引值
+  });
+};
+
+export const removeSortItemById = (
+  array: SortIdItem[],
+  idToRemove: number
+): SortIdItem[] => {
+  return array.filter((item) => item.id !== idToRemove);
+};
