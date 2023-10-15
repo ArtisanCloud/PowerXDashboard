f71623704dd4ace21bd4cf9d396f92b1368ac804
Matrix-X <hbj418@gmail.com>
2023-10-15 19:55:01 +0800

    refact(version): change version location

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -7,7 +7,7 @@
           :style="{ margin: 0, fontSize: '18px' }"
           :heading="5"
         >
-          PowerX Dashboard - {{ PowerXVersion }}
+          PowerX Dashboard
         </a-typography-title>
         <icon-menu-fold
           v-if="!topMenu && appStore.device === 'mobile'"
