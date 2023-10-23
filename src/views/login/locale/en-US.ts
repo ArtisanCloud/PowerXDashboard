3bd193636572968aa2c4cad3bea81d5393e0e95c
Matrix-X <hbj418@gmail.com>
2023-10-23 11:29:52 +0800

    feat(crm): add uuid for customer

--- src/views/login/locale/en-US.ts
+++ src/views/login/locale/en-US.ts
@@ -1,5 +1,5 @@
 export default {
-  'login.form.title': 'Login to 小裂匠',
+  'login.form.title': 'Login to PowerX',
   'login.form.userName.errMsg': 'Username cannot be empty',
   'login.form.password.errMsg': 'Password cannot be empty',
   'login.form.login.errMsg': 'Login error, refresh and try again',
--- src/views/login/locale/zh-CN.ts
+++ src/views/login/locale/zh-CN.ts
@@ -1,5 +1,5 @@
 export default {
-  'login.form.title': '登录 小裂匠',
+  'login.form.title': '登录 PowerX',
   'login.form.userName.errMsg': '用户名不能为空',
   'login.form.password.errMsg': '密码不能为空',
   'login.form.login.errMsg': '登录出错，请刷新重试',
